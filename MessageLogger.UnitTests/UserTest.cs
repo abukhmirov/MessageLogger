379abfc843a96616081c08e265bf1a2545934cb7
abukhmirov <alexbukhmirov@gmail.com>
2023-04-26 13:19:47 -0600

    Update 3 Iteration 2 Done!!!

--- MessageLogger.UnitTests/UserTest.cs
+++ MessageLogger.UnitTests/UserTest.cs
@@ -11,11 +11,14 @@
             Assert.Equal(new List<Message>(), John.UserMessages);
         }
         [Fact]
-        public void User_AddMessageToList()
+        public void User_AddMessageToList_MessageAdded()
         {
             var userTested = new User("Tester", "tester");
             var messageTest = new Message("Hello");
+            var messageTest2 = new Message("Hello2");
             userTested.AddMessagetoList(messageTest);
+            Assert.Equal(messageTest, userTested.UserMessages[0]);
+            Assert.Equal(messageTest2, userTested.UserMessages[1]);
 
         }
     }
--- MessageLogger/Program.cs
+++ MessageLogger/Program.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using MessageLogger;
 
-
-
-var Users = new List<User>();
-var Messages = new List<Message>();
+var Users = new Dictionary<string, User>();
 
 User user1 = new User("user", "name");
 
@@ -15,49 +12,75 @@ user1.Name = Console.ReadLine();
 Console.Write("What is your username? (one word, no spaces!) ");
 user1.Username = Console.ReadLine();
 
+Users.Add(user1.Username, user1);
+
 Console.WriteLine("To log out of your user profile, enter 'log out'. To quit the application, enter 'quit')");
-Console.Write("Add a message: ");// prog
 
-var userInput = Console.ReadLine(); // prog
-
-Message message = new Message(userInput); // prog
-
-
-
-
-if (userInput != "quit")
-{
-    Messages.Add(message);
-    user1.AddMessagetoList(message);
-    Console.WriteLine(message.CreatedAt.ToString("h:mm tt") + ": " + message.Content);
-}
-else if (userInput != "log out")
-{
-    Console.Write("Would you like to log in");
-}
+Console.Write("Add a message: ");
+var userInput = Console.ReadLine();
 
 while (userInput != "quit")
 {
-    Console.Write("Add message: ");
-   
-    userInput = Console.ReadLine();
-   
-    Message nextMessage = new Message(userInput);
-   
-    if (userInput != "quit")
-    { 
-        Messages.Add(nextMessage);
-        user1.AddMessagetoList(message);
-    }
-   
-    foreach (var userMessage in Messages)
+    if (userInput != "log out")
     {
+        // Add message to current user
+        Message message = new Message(userInput);
+        user1.AddMessagetoList(message);
+        //Console.WriteLine(message.CreatedAt.ToString("h:mm tt") + ": " + message.Content);
+        foreach (var userMessage in user1.UserMessages)
+        {
+            Console.WriteLine(userMessage.CreatedAt.ToString("h:mm tt") + ": " + userMessage.Content);
+        }
 
-        Console.WriteLine(userMessage.CreatedAt.ToString("h:mm tt") + ": " + userMessage.Content);
     }
+    else
+    {
+        // Log out and log in as a new or existing user
+        Console.Write("Would you like to log in a 'new' or 'existing' user?");
+        userInput = Console.ReadLine();
+
+        if (userInput == "new")
+        {
+            User userNew = new User("user", "name");
+
+            Console.Write("What is your name? ");
+            userNew.Name = Console.ReadLine();
+
+            Console.Write("What is your username? (one word, no spaces!) ");
+            userNew.Username = Console.ReadLine();
+
+            Users.Add(userNew.Username, userNew);
+            user1 = userNew;
+        }
+        else if (userInput == "existing")
+        {
+            Console.Write("Enter your username: ");
+            userInput = Console.ReadLine();
+
+            if (Users.ContainsKey(userInput))
+            {
+                user1 = Users[userInput];
+            }
+            else
+            {
+                Console.WriteLine("User not found.");
+            }
+        }
+
+        // Display all messages of the current user
+        //Console.WriteLine("Messages for " + user1.Name + " (" + user1.Username + "):");
+        //foreach (var userMessage in user1.UserMessages)
+        //{
+        //    Console.WriteLine(userMessage.CreatedAt.ToString("h:mm tt") + ": " + userMessage.Content);
+        //}
+    }
+
+    Console.Write("Add a message: ");
+    userInput = Console.ReadLine();
+}
+foreach (var user in Users.Values)
+{
+    Console.WriteLine(user.Name + " wrote " + user.UserMessages.Count + " messages");
 }
 
-
-Console.WriteLine("Thanks for using Message Logger! During this session, you logged " + user1.UserMessages.Count + " messages");
-
-
+//Console.WriteLine("Thanks for using Message Logger!" + user1.UserMessages.Count + " messages");
